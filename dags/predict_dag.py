c739a9547e222ab79da850294925a86c98861e54
iwai9955 <62748104+iwai9955@users.noreply.github.com>
2025-06-21 07:43:59 +1000

    Initial commit

---
+++ dags/predict_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import sys
+sys.path.insert(0, "/opt/airflow/scripts")
+from predict_model import predict_ev_demand
+
+default_args = {
+    'owner': 'soobok',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 6, 13),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=10),
+}
+
+dag = DAG(
+    dag_id='predict_ev_demand_daily',
+    default_args=default_args,
+    description='Daily EV demand prediction DAG',
+    schedule='0 8 * * *',  # ✅ 수정됨
+    catchup=False,
+)
+
+predict_task = PythonOperator(
+    task_id='predict_ev_demand',
+    python_callable=predict_ev_demand,
+    dag=dag,
+)
